418e8071419fdb51d7ad341b46db8e1af22c1131
haswanth-k <40890509+haswanth-k@users.noreply.github.com>
2024-01-23 13:55:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,10 +42,7 @@ size=var.vm-size
 admin_username = var.user-name
 admin_password = var.password
 network_interface_ids = [azurerm_network_interface.windows-nic.id]
-  tags = {
-    Environemnt : "Development"
-    Development : "phase1"
-  }
+tags = var.tags
 
 # os-disk creation
 os_disk {
